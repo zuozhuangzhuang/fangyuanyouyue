bb3828dac5cbea5475c968b417bf2cde37ed70af
左壮壮 <zuozhuang_zzz@163.com>
2018-05-14 16:23:57 +0800

    初次提交，请多包涵

---
+++ business-system/forum-service/src/main/java/com/fangyuanyouyue/forum/ForumServiceApplication.java
@@ -0,0 +1,12 @@
+package com.fangyuanyouyue.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumServiceApplication.class, args);
+	}
+}
