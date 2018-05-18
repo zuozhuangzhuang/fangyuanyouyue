2cd01e0d991b1abc8d8a76e97042a2b57fddd8b0
左壮壮 <zuozhuang_zzz@163.com>
2018-05-18 15:05:07 +0800

    删掉了获取用户列表的方法

--- business-system/user-service/src/main/java/com/fangyuanyouyue/user/service/impl/UserServiceImpl.java
+++ business-system/user-service/src/main/java/com/fangyuanyouyue/user/service/impl/UserServiceImpl.java
@@ -3,24 +3,13 @@ package com.fangyuanyouyue.user.service.impl;
 import com.fangyuanyouyue.user.dao.UserMapper;
 import com.fangyuanyouyue.user.model.User;
 import com.fangyuanyouyue.user.service.UserService;
-import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service(value = "userService")
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
-
-
-    @Override
-    public List<User> getList(int pageNum, int pageSize) {
-        PageHelper.startPage(pageNum, pageSize);
-        return userMapper.getList();
-    }
-
 
     @Override
     public User selectByPrimaryKey(Integer id) {
