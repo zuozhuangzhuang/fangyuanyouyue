8783e7a33405b1a586ed5cfdc4fecfb3abf9a1da
左壮壮 <zuozhuang_zzz@163.com>
2018-05-18 12:52:42 +0800

    zuul-server修改启动类注解，否则会连接超时，统一swagger接口页面到zuul网关端口

--- based-framework/zuul-server/src/main/java/com/fangyuanyouyue/zuul/ZuulServerApplication.java
+++ based-framework/zuul-server/src/main/java/com/fangyuanyouyue/zuul/ZuulServerApplication.java
@@ -1,18 +1,12 @@
 package com.fangyuanyouyue.zuul;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableDiscoveryClient
 @EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class ZuulServerApplication {
 
