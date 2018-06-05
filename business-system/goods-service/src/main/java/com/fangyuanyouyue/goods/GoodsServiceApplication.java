14a505bf6546ab11ad83b2d8ef14ca9e3ea9baad
左壮壮 <zuozhuang_zzz@163.com>
2018-06-05 16:15:54 +0800

    增加商品系统的相关代码

--- business-system/goods-service/src/main/java/com/fangyuanyouyue/goods/GoodsServiceApplication.java
+++ business-system/goods-service/src/main/java/com/fangyuanyouyue/goods/GoodsServiceApplication.java
@@ -1,30 +1,44 @@
 package com.fangyuanyouyue.goods;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
-@RestController
+@EnableSwagger2
+@RefreshScope
 public class GoodsServiceApplication {
+	/**
+	 * ************************************************************************
+	 * **                              _oo0oo_                               **
+	 * **                             o8888888o                              **
+	 * **                             88" . "88                              **
+	 * **                             (| -_- |)                              **
+	 * **                             0\  =  /0                              **
+	 * **                           ___/'---'\___                            **
+	 * **                        .' \\\|     |// '.                          **
+	 * **                       / \\\|||  :  |||// \\                        **
+	 * **                      / _ ||||| -:- |||||- \\                       **
+	 * **                      | |  \\\\  -  /// |   |                       **
+	 * **                      | \_|  ''\---/''  |_/ |                       **
+	 * **                      \  .-\__  '-'  __/-.  /                       **
+	 * **                    ___'. .'  /--.--\  '. .'___                     **
+	 * **                 ."" '<  '.___\_<|>_/___.' >'  "".                  **
+	 * **                | | : '-  \'.;'\ _ /';.'/ - ' : | |                 **
+	 * **                \  \ '_.   \_ __\ /__ _/   .-' /  /                 **
+	 * **            ====='-.____'.___ \_____/___.-'____.-'=====             **
+	 * **                              '=---='                               **
+	 * ************************************************************************
+	 * **                        佛祖保佑      启动正常                      **
+	 * ************************************************************************
+	 *
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoodsServiceApplication.class, args);
-	}
-	private static final Logger LOG = Logger.getLogger(GoodsServiceApplication.class.getName());
-	@Value("${server.port}")
-	String port;
-	@RequestMapping("/goods")
-	public String forum(@RequestParam String name) {
-		LOG.log(Level.INFO, "calling trace goods-service  ");
-		return "goods "+name+",i am from port:" +port;
 	}
 }
