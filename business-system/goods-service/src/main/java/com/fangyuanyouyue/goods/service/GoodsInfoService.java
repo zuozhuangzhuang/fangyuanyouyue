1491e78bb7999edf0e64ba5ed708822ffc94179a
左壮壮 <zuozhuang_zzz@163.com>
2018-07-01 17:23:13 +0800

    商品相关接口

---
+++ business-system/goods-service/src/main/java/com/fangyuanyouyue/goods/service/GoodsInfoService.java
@@ -0,0 +1,40 @@
+package com.fangyuanyouyue.goods.service;
+
+import com.fangyuanyouyue.goods.model.GoodsInfo;
+import com.fangyuanyouyue.goods.param.GoodsParam;
+import com.fangyuanyouyue.goods.utils.ServiceException;
+
+import java.util.List;
+
+public interface GoodsInfoService {
+
+    /**
+     * 根据ID获取商品
+     * @param id
+     * @return
+     */
+    GoodsInfo selectByPrimaryKey(Integer id);
+
+    /**
+     * 获取商品列表
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    List<GoodsInfo> getGoodsInfoList(int pageNum, int pageSize) throws ServiceException;
+
+    /**
+     * 新增商品
+     * @param param
+     * @return
+     * @throws ServiceException
+     */
+    GoodsInfo addGoods(Integer userId,String nickName,GoodsParam param) throws ServiceException;
+
+    /**
+     * 批量删除商品
+     * @param goodsIds
+     * @throws ServiceException
+     */
+    void deleteGoods(Integer[] goodsIds) throws ServiceException;
+}
