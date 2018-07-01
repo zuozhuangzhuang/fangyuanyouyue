23c8cdc8ff3625aca79228f21eeb849b542b6702
左壮壮 <zuozhuang_zzz@163.com>
2018-07-01 17:09:17 +0800

    type改成int类型

--- business-system/goods-service/src/main/java/com/fangyuanyouyue/goods/param/BaseParam.java
+++ business-system/goods-service/src/main/java/com/fangyuanyouyue/goods/param/BaseParam.java
@@ -9,24 +9,24 @@ import io.swagger.annotations.ApiModelProperty;
 @ApiModel(value = "后台请求基础参数")
 public class BaseParam {
 
-	@ApiModelProperty(name = "start", value = "起始页", dataType = "Integer")
+	@ApiModelProperty(name = "start", value = "起始页", dataType = "Integer",hidden = true)
 	private Integer start; // 起始页
-	@ApiModelProperty(name = "limit", value = "限制页", dataType = "Integer")
+	@ApiModelProperty(name = "limit", value = "限制页", dataType = "Integer",hidden = true)
 	private Integer limit; // 限制页
-	@ApiModelProperty(name = "name", value = "搜素字段", dataType = "Integer")
+	@ApiModelProperty(name = "name", value = "搜素字段", dataType = "Integer",hidden = true)
 	private String name; // 搜素字段
-	@ApiModelProperty(name = "type", value = "类型", dataType = "String")
-	private String type;//类型
+	@ApiModelProperty(name = "type", value = "类型", dataType = "Integer",hidden = true)
+	private Integer type;//类型 goodsInfo：1普通商品 2秒杀商品 goodsCategory：1主图 2次图 goodsCategory：1普通 2热门
 
 	public BaseParam() {
 
 	}
 
-	public String getType() {
+	public Integer getType() {
 		return type;
 	}
 
-	public void setType(String type) {
+	public void setType(Integer type) {
 		this.type = type;
 	}
 
