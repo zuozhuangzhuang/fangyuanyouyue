44172d26a3bc97a494ad9c0566652ff98210e1d3
左壮壮 <zuozhuang_zzz@163.com>
2018-07-04 18:52:03 +0800

    增加了小程序相关字段和修改了性别判断

--- business-system/user-service/src/main/java/com/fangyuanyouyue/user/param/UserParam.java
+++ business-system/user-service/src/main/java/com/fangyuanyouyue/user/param/UserParam.java
@@ -9,84 +9,121 @@ import java.util.Date;
 public class UserParam extends BaseParam {
 	@ApiModelProperty(name = "phone", value = "手机号码", dataType = "String",hidden = true)
 	private String phone;//手机号码
+
 	@ApiModelProperty(name = "email", value = "电子邮件", dataType = "String",hidden = true)
 	private String email;//电子邮件
+
 	@ApiModelProperty(name = "userAddress", value = "用户所在地", dataType = "String",hidden = true)
 	private String userAddress;//用户所在地
+
 	@ApiModelProperty(name = "loginPwd", value = "登录密码,MD5小写", dataType = "String",hidden = true)
 	private String loginPwd;//登录密码,MD5小写
+
 	@ApiModelProperty(name = "nickName", value = "昵称", dataType = "String",hidden = true)
 	private String nickName;//昵称
+
 	@ApiModelProperty(name = "headImgUrl", value = "头像图片地址", dataType = "String",hidden = true)
 	private String headImgUrl;//头像图片地址
+
 	@ApiModelProperty(name = "headImg", value = "头像图片", dataType = "file",hidden = true)
 	private MultipartFile headImg;//头像图片
+
 	@ApiModelProperty(name = "bgImgUrl", value = "背景图片地址", dataType = "String",hidden = true)
 	private String bgImgUrl;//背景图片地址
+
 	@ApiModelProperty(name = "bgImg", value = "背景图片", dataType = "file",hidden = true)
 	private MultipartFile bgImg;//背景图片
-	@ApiModelProperty(name = "gender", value = "性别，0女 1男 2不确定", dataType = "int",hidden = true)
-	private Integer gender;//性别，0女 1男 2不确定
+
+	@ApiModelProperty(name = "gender", value = "性别，1男 2女 0不确定", dataType = "int",hidden = true)
+	private Integer gender;//性别，1男 2女 0不确定
+
 	@ApiModelProperty(name = "signature", value = "个性签名", dataType = "String",hidden = true)
 	private String signature;//个性签名
+
 	@ApiModelProperty(name = "contact", value = "联系电话", dataType = "String",hidden = true)
 	private String contact;//联系电话
+
 	@ApiModelProperty(name = "regType", value = "注册来源 1app 2微信小程序", dataType = "int",hidden = true)
 	private Integer regType;//注册来源 1app 2微信小程序
+
 	@ApiModelProperty(name = "regPlatform", value = "注册平台 1安卓 2IOS 3小程序", dataType = "int",hidden = true)
 	private Integer regPlatform;//注册平台 1安卓 2IOS 3小程序
+
 	@ApiModelProperty(name = "levelDesc", value = "等级描述", dataType = "String",hidden = true)
 	private String levelDesc;//等级描述
+
 	@ApiModelProperty(name = "status", value = "状态", dataType = "int",hidden = true)
 	private Integer status;//用户信息状态 1正常 2冻结 / 用户扩展实名登记状态 1已实名 2未实名 / 用户会员状态1已开通 2未开通 / 实名认证状态 1申请 2通过 3拒绝
+
 	@ApiModelProperty(name = "updateTime", value = "更新时间", dataType = "int",hidden = true)
 	private Date updateTime;//更新时间
+
 	@ApiModelProperty(name = "userId", value = "用户ID", dataType = "int",hidden = true)
 	private Integer userId;//用户ID
+
 	@ApiModelProperty(name = "newPwd", value = "新密码", dataType = "String",hidden = true)
 	private String newPwd;//新密码
+
 	@ApiModelProperty(name = "birth", value = "生日", dataType = "String",hidden = true)
 	private String birth;//生日
+
 	@ApiModelProperty(name = "name", value = "真实姓名", dataType = "String",hidden = true)
 	private String name;//真实姓名
+
 	@ApiModelProperty(name = "identity", value = "身份证号码", dataType = "String",hidden = true)
 	private String identity;//身份证号码
+
 	@ApiModelProperty(name = "payPwd", value = "支付密码，md5加密，32位小写字母", dataType = "String",hidden = true)
 	private String payPwd;//支付密码，明文6位，MD5小写
+
 	@ApiModelProperty(name = "identityImgCover", value = "身份证封面图", dataType = "String",hidden = true)
 	private String identityImgCover;//身份证封面图
+
 	@ApiModelProperty(name = "identityImgBack", value = "身份证背面", dataType = "String",hidden = true)
 	private String identityImgBack;//身份证背面
+
 	@ApiModelProperty(name = "receiverName", value = "收货人", dataType = "String",hidden = true)
 	private String receiverName;//收货人
+
 	@ApiModelProperty(name = "receiverPhone", value = "收货人手机号码", dataType = "String",hidden = true)
 	private String receiverPhone;//收货人手机号码
+
 	@ApiModelProperty(name = "province", value = "省", dataType = "String",hidden = true)
 	private String province;//省
+
 	@ApiModelProperty(name = "city", value = "市", dataType = "String",hidden = true)
 	private String city;//市
+
 	@ApiModelProperty(name = "area", value = "区", dataType = "String",hidden = true)
 	private String area;//区
+
 	@ApiModelProperty(name = "address", value = "详细地址", dataType = "String",hidden = true)
 	private String address;//详细地址
+
 	@ApiModelProperty(name = "postCode", value = "邮编", dataType = "String",hidden = true)
 	private String postCode;//邮编
+
 	@ApiModelProperty(name = "addressId", value = "地址ID", dataType = "int",hidden = true)
 	private Integer addressId;//地址ID
+
 	@ApiModelProperty(name = "unionId", value = "第三方唯一ID", dataType = "String",hidden = true)
 	private String unionId;//第三方唯一ID
-	@ApiModelProperty(name = "appOpenId", value = "微信app openid", dataType = "String",hidden = true)
-	private String appOpenId;//微信app openid
-	@ApiModelProperty(name = "mpOpenId", value = "公众号openid", dataType = "String",hidden = true)
-	private String mpOpenId;//公众号openid
-	@ApiModelProperty(name = "miniOpenId", value = "小程序openid", dataType = "String",hidden = true)
-	private String miniOpenId;//小程序openid
+
 	@ApiModelProperty(name = "thirdNickName", value = "第三方账号昵称", dataType = "String",hidden = true)
 	private String thirdNickName;//第三方账号昵称
+
 	@ApiModelProperty(name = "thirdHeadImgUrl", value = "第三方账号头像地址", dataType = "String",hidden = true)
 	private String thirdHeadImgUrl;//第三方账号头像地址
-	@ApiModelProperty(name = "lastLoginPlatform", value = "最后登录平台 1安卓 2IOS 3小程序", dataType = "int",hidden = true)
-	private Integer lastLoginPlatform;//最后登录平台 1安卓 2IOS 3小程序
+
+	@ApiModelProperty(name = "loginPlatform", value = "最后登录平台 1安卓 2IOS 3小程序", dataType = "int",hidden = true)
+	private Integer loginPlatform;//最后登录平台 1安卓 2IOS 3小程序
+
+	@ApiModelProperty(name = "code", value = "微信获取的code", dataType = "String",hidden = true)
+	private String code;//微信获取的code
+	@ApiModelProperty(name = "encryptedData", value = "包括敏感数据在内的完整用户信息的加密数据", dataType = "String",hidden = true)
+	private String encryptedData;//包括敏感数据在内的完整用户信息的加密数据
+	@ApiModelProperty(name = "iv", value = "加密算法的初始向量", dataType = "String",hidden = true)
+	private String iv;//加密算法的初始向量
 
 	public String getPhone() {
 		return phone;
@@ -338,30 +375,6 @@ public class UserParam extends BaseParam {
 		this.unionId = unionId;
 	}
 
-	public String getAppOpenId() {
-		return appOpenId;
-	}
-
-	public void setAppOpenId(String appOpenId) {
-		this.appOpenId = appOpenId;
-	}
-
-	public String getMpOpenId() {
-		return mpOpenId;
-	}
-
-	public void setMpOpenId(String mpOpenId) {
-		this.mpOpenId = mpOpenId;
-	}
-
-	public String getMiniOpenId() {
-		return miniOpenId;
-	}
-
-	public void setMiniOpenId(String miniOpenId) {
-		this.miniOpenId = miniOpenId;
-	}
-
 	public String getThirdNickName() {
 		return thirdNickName;
 	}
@@ -378,13 +391,6 @@ public class UserParam extends BaseParam {
 		this.thirdHeadImgUrl = thirdHeadImgUrl;
 	}
 
-	public Integer getLastLoginPlatform() {
-		return lastLoginPlatform;
-	}
-
-	public void setLastLoginPlatform(Integer lastLoginPlatform) {
-		this.lastLoginPlatform = lastLoginPlatform;
-	}
 
 	public String getPayPwd() {
 		return payPwd;
@@ -408,6 +414,38 @@ public class UserParam extends BaseParam {
 
 	public void setBgImg(MultipartFile bgImg) {
 		this.bgImg = bgImg;
+	}
+
+	public Integer getLoginPlatform() {
+		return loginPlatform;
+	}
+
+	public void setLoginPlatform(Integer loginPlatform) {
+		this.loginPlatform = loginPlatform;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getEncryptedData() {
+		return encryptedData;
+	}
+
+	public void setEncryptedData(String encryptedData) {
+		this.encryptedData = encryptedData;
+	}
+
+	public String getIv() {
+		return iv;
+	}
+
+	public void setIv(String iv) {
+		this.iv = iv;
 	}
 
 	@Override
@@ -447,12 +485,12 @@ public class UserParam extends BaseParam {
 				", postCode='" + postCode + '\'' +
 				", addressId=" + addressId +
 				", unionId='" + unionId + '\'' +
-				", appOpenId='" + appOpenId + '\'' +
-				", mpOpenId='" + mpOpenId + '\'' +
-				", miniOpenId='" + miniOpenId + '\'' +
 				", thirdNickName='" + thirdNickName + '\'' +
 				", thirdHeadImgUrl='" + thirdHeadImgUrl + '\'' +
-				", lastLoginPlatform=" + lastLoginPlatform +
+				", loginPlatform=" + loginPlatform +
+				", code='" + code + '\'' +
+				", encryptedData='" + encryptedData + '\'' +
+				", iv='" + iv + '\'' +
 				'}';
 	}
 }
