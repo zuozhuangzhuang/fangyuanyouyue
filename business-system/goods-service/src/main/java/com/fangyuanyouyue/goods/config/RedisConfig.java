ccfefdd02e5917fb0dc5bd722c563a9a04446a6e
左壮壮 <zuozhuang_zzz@163.com>
2018-07-10 16:47:18 +0800

    Redis配置

--- business-system/goods-service/src/main/java/com/fangyuanyouyue/goods/config/RedisConfig.java
+++ business-system/goods-service/src/main/java/com/fangyuanyouyue/goods/config/RedisConfig.java
@@ -1,35 +1,56 @@
-package com.fangyuanyouyue.goods.config;//package com.fangyuanyouyue.auth.config;
-//
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import redis.clients.jedis.JedisPool;
-//import redis.clients.jedis.JedisPoolConfig;
-//
-//@Configuration
-//public class RedisConfig{
-//
-//    @Bean(name= "jedis.pool")
-//    @Autowired
-//    public JedisPool jedisPool(@Qualifier("jedis.pool.config") JedisPoolConfig config,
-//                               @Value("${jedis.pool.host}")String host,
-//                               @Value("${jedis.pool.port}")int port) {
-//        return new JedisPool(config, host, port);
-//    }
-//
-//    @Bean(name= "jedis.pool.config")
-//    public JedisPoolConfig jedisPoolConfig (@Value("${jedis.pool.config.maxTotal}")int maxTotal,
-//                                            @Value("${jedis.pool.config.maxIdle}")int maxIdle,
-//                                            @Value("${jedis.pool.config.maxWaitMillis}")int maxWaitMillis) {
-//        JedisPoolConfig config = new JedisPoolConfig();
-//        config.setMaxTotal(maxTotal);
-//        config.setMaxIdle(maxIdle);
-//        config.setMaxWaitMillis(maxWaitMillis);
-//        return config;
-//    }
-//
-//}
+package com.fangyuanyouyue.goods.config;
+import org.apache.log4j.Logger;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * Redis配置文件类
+ *
+ */
+public class RedisConfig {
+    private static Logger logger = Logger.getLogger(RedisConfig.class);
+
+    @Bean
+    @ConfigurationProperties(prefix="spring.redis.pool")
+    public JedisPoolConfig getRedisConfig(){
+        JedisPoolConfig config = new JedisPoolConfig();
+        return config;
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix="spring.redis")
+    public JedisConnectionFactory getConnectionFactory(){
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        JedisPoolConfig config = getRedisConfig();
+        factory.setPoolConfig(config);
+        logger.info("JedisConnectionFactory bean init success.");
+        return factory;
+    }
+
+    @Bean
+    @Primary
+    public RedisTemplate redisTemplate() {
+        JedisConnectionFactory jedisConnectionFactory = getConnectionFactory();
+        StringRedisTemplate template = new StringRedisTemplate(jedisConnectionFactory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        jedisConnectionFactory.afterPropertiesSet();
+        return template;
+    }
+}
