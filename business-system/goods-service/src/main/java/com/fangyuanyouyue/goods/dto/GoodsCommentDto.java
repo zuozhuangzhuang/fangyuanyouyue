960dbe4db4db7fa5affa878e7ac59aad16209b50
左壮壮 <zuozhuang_zzz@163.com>
2018-07-16 12:42:39 +0800

    商品评论相关逻辑

--- business-system/goods-service/src/main/java/com/fangyuanyouyue/goods/dto/GoodsCommentDto.java
+++ business-system/goods-service/src/main/java/com/fangyuanyouyue/goods/dto/GoodsCommentDto.java
@@ -4,13 +4,27 @@ import com.fangyuanyouyue.goods.model.GoodsComment;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 商品评论Dto
  */
 public class GoodsCommentDto {
     //GoodsComment
-    private Integer userId;//用户id
+    private Integer id;//评论ID
+
+    private Integer fromUserId;//发表评论用户id
+
+    private String fromUserName;//发表评论用户昵称
+
+    private String fromUserHeadImgUrl;//发表评论用户头像
+
+    private Integer toUserId;//被评论用户id
+
+    private String toUserName;//被评论用户昵称
+
+    private String toUserHeadImgUrl;//被评论用户头像
+
 
     private Integer commentId;//回复评论id
 
@@ -26,36 +40,44 @@ public class GoodsCommentDto {
 
     private Integer status;//状态 1正常 2隐藏
 
+    private List<GoodsCommentDto> replys;//评论下的回复
+
     public GoodsCommentDto() {
     }
 
-    public GoodsCommentDto(GoodsComment goodsComment) {
-        this.userId = goodsComment.getUserId();
-        this.commentId = goodsComment.getCommentId();
-        this.content = goodsComment.getContent();
-        this.likesCount = goodsComment.getLikesCount();
-        this.img1Url = goodsComment.getImg1Url();
-        this.img2Url = goodsComment.getImg2Url();
-        this.img3Url = goodsComment.getImg3Url();
-        this.status = goodsComment.getStatus();
+
+    public GoodsCommentDto(Map map) {
+        this.id = (Integer)map.get("id");
+        this.fromUserId = (Integer)map.get("user_id");
+        this.fromUserName = (String)map.get("nick_name");
+        this.fromUserHeadImgUrl = (String)map.get("head_img_url");
+        this.commentId = (Integer)map.get("comment_id");
+        this.content = (String)map.get("content");
+        this.likesCount = (Integer)map.get("likes_count");
+        this.img1Url = (String)map.get("img1_url");
+        this.img2Url = (String)map.get("img2_url");
+        this.img3Url = (String)map.get("img3_url");
+        this.status = (Integer)map.get("status");
     }
-    public static List<GoodsCommentDto> toDtoList(List<GoodsComment> list) {
-        if (list == null)
+
+
+    public static List<GoodsCommentDto> mapToDtoList(List<Map<String,Object>> maps) {
+        if (maps == null)
             return null;
         List<GoodsCommentDto> dtolist = new ArrayList<>();
-        for (GoodsComment model : list) {
+        for (Map<String,Object> model : maps) {
             GoodsCommentDto dto = new GoodsCommentDto(model);
             dtolist.add(dto);
         }
         return dtolist;
     }
 
-    public Integer getUserId() {
-        return userId;
+    public Integer getId() {
+        return id;
     }
 
-    public void setUserId(Integer userId) {
-        this.userId = userId;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public Integer getCommentId() {
@@ -114,4 +136,59 @@ public class GoodsCommentDto {
         this.status = status;
     }
 
+    public List<GoodsCommentDto> getReplys() {
+        return replys;
+    }
+
+    public void setReplys(List<GoodsCommentDto> replys) {
+        this.replys = replys;
+    }
+
+    public Integer getFromUserId() {
+        return fromUserId;
+    }
+
+    public void setFromUserId(Integer fromUserId) {
+        this.fromUserId = fromUserId;
+    }
+
+    public String getFromUserName() {
+        return fromUserName;
+    }
+
+    public void setFromUserName(String fromUserName) {
+        this.fromUserName = fromUserName;
+    }
+
+    public String getFromUserHeadImgUrl() {
+        return fromUserHeadImgUrl;
+    }
+
+    public void setFromUserHeadImgUrl(String fromUserHeadImgUrl) {
+        this.fromUserHeadImgUrl = fromUserHeadImgUrl;
+    }
+
+    public Integer getToUserId() {
+        return toUserId;
+    }
+
+    public void setToUserId(Integer toUserId) {
+        this.toUserId = toUserId;
+    }
+
+    public String getToUserName() {
+        return toUserName;
+    }
+
+    public void setToUserName(String toUserName) {
+        this.toUserName = toUserName;
+    }
+
+    public String getToUserHeadImgUrl() {
+        return toUserHeadImgUrl;
+    }
+
+    public void setToUserHeadImgUrl(String toUserHeadImgUrl) {
+        this.toUserHeadImgUrl = toUserHeadImgUrl;
+    }
 }
